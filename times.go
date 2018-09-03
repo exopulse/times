5363ca7d33f84b397dba0e9a1e4abf6652cd52ee
Alan Bunjevac <alan.bunjevac@exobyte.org>
2018-09-03 11:01:16 +0200

    Detect timezone format

--- times.go
+++ times.go
@@ -2,7 +2,13 @@ package times
 
 import "time"
 
-// Parse parses time using format auto-detection. At the moment RFC3339 format is assumed.
+// Parse parses time using format auto-detection.
 func Parse(value string) (time.Time, error) {
-	return time.Parse("2006-01-02T15:04:05.000Z", value)
+	t, err := time.Parse(time.RFC3339, value)
+
+	if err != nil {
+		return time.Parse("2006-01-02T15:04:05-0700", value)
+	}
+
+	return t, nil
 }
