7bc03dd15f011031ce4cf868aebdf9c51ef538ab
Alan Bunjevac <alan.bunjevac@exobyte.org>
2018-09-05 14:39:41 +0200

    Must version of Parse

--- times.go
+++ times.go
@@ -1,6 +1,9 @@
 package times
 
-import "time"
+import (
+	"log"
+	"time"
+)
 
 // Parse parses time using format auto-detection.
 func Parse(value string) (time.Time, error) {
@@ -11,4 +14,15 @@ func Parse(value string) (time.Time, error) {
 	}
 
 	return t, nil
+}
+
+// MustParse is a helper that wraps a call to Parse() function and panics if the error is non-nil.
+func MustParse(value string) time.Time {
+	time, e := Parse(value)
+
+	if e != nil {
+		log.Panicf("failed to parse time [%s]: %s", value, e)
+	}
+
+	return time
 }
